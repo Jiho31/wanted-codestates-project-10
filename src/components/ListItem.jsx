69afa530ba324ef7fbf02a9fcd48edb42653208c
Jiho Bok <bok.jiho@gmail.com>
2022-03-25 12:00:08 +0900

    [Design] 커서 올리면 선택된 추천 검색어 영역 배경색 적용

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,19 +2,42 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as Search } from '../assets/search-icon.svg';
 
-function ListItem({ keyword, isActive }) {
+const ListItem = React.memo(function ListItem({
+  keyword,
+  isActive,
+  setActiveIndex,
+  index,
+}) {
   useEffect(() => {
     // console.log(index + 'list item is rendered');
     // console.log(isActive);
   });
 
+  const mouseEnterHandler = (e) => {
+    // console.log(e.target.querySelectorAll('p')[0].textContent);
+    // console.log(e.target.querySelectorAll('.hidden-index')[0].textContent);
+
+    setActiveIndex(
+      e.target.querySelectorAll('.hidden-index')[0].textContent * 1,
+    );
+  };
+
+  const clickEventHandler = (e) => {
+    // input의 keyword 값 바꾸기!
+  };
+
   return (
-    <List className={isActive ? 'isActive' : ''}>
+    <List
+      className={isActive ? 'isActive' : ''}
+      onMouseEnter={mouseEnterHandler}
+      onClick={clickEventHandler}
+    >
       <Search />
       <p>{keyword.name}</p>
+      <Index className="hidden-index">{index}</Index>
     </List>
   );
-}
+});
 
 const List = styled.li`
   display: flex;
@@ -32,6 +55,9 @@ const List = styled.li`
   :hover {
     background-color: #efefef;
   }
+`;
+const Index = styled.div`
+  visibility: hidden;
 `;
 
 export default ListItem;
