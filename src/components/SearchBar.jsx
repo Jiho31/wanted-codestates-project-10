81dd0d3c9156b275d5f6a2e371a90a7031c1db48
Jiho Bok <bok.jiho@gmail.com>
2022-03-26 00:18:02 +0900

    [Feat] #2 api 호출별로 로컬 캐싱 구현

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,17 +14,38 @@ const SearchBar = React.memo(function SearchBar({
   const dispatch = useDispatch();
   let timerID;
 
+  const checkLocalCache = (newKeyword) => {
+    if (window.localStorage.getItem('searchKeywords')) {
+      const localCache = JSON.parse(
+        window.localStorage.getItem('searchKeywords'),
+      );
+
+      return localCache.hasOwnProperty(newKeyword);
+    } else return false;
+  };
+
   const inputChangeHandler = useCallback((e) => {
-    dispatch(setSearchKeyword(e.target.value));
+    const newKeyword = e.target.value;
+    dispatch(setSearchKeyword(newKeyword));
 
     handleVisibility('show');
 
     // 디바운싱 적용한 api 호출 함수 실행
     clearTimeout(timerID);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     timerID = setTimeout(() => {
-      fetchKeywordAPI(e.target.value);
-      // console.log('api 호출');
+      // 로컬 캐시에 키워드 검색 결과가 존재하는지 확인
+      if (checkLocalCache(newKeyword)) {
+        const localCache = JSON.parse(
+          window.localStorage.getItem('searchKeywords'),
+        );
+        dispatch(setRecommendedList(localCache[newKeyword]));
+      } else {
+        // API 호출
+        fetchKeywordAPI(newKeyword);
+        // console.log('api 호출');
+      }
     }, 350);
   }, []);
 
@@ -45,7 +66,21 @@ const SearchBar = React.memo(function SearchBar({
       })
       .catch((err) => console.error(err));
 
+    // 리덕스 상태 값 업데이트
     dispatch(setRecommendedList(recommendedKeywords));
+
+    // 로컬 스토리지에 값 저장
+    const dataFromStorage = JSON.parse(
+      window.localStorage.getItem('searchKeywords'),
+    );
+    window.localStorage.setItem(
+      'searchKeywords',
+      JSON.stringify({
+        ...dataFromStorage,
+        [newKeyword]: recommendedKeywords,
+      }),
+    );
+
     setIsLoading(false);
   };
 
